755f53fbcf99fdc0b8cdb21b86fe2deb2daa6600
MehdiSV <mehdisv@gmail.com>
2015-01-06 22:07:57 +0100

    Init Project

---
+++ BelArt/BelArt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BelArt
+//
+//  Created by Mehdi Sqalli on 06/01/2015.
+//  Copyright (c) 2015 Mehdi Sqalli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
