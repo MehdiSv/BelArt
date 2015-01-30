638189e0b915877132d362f1f0b5b7fcd338852f
Mehdi Sqalli <mehdisv@gmail.com>
2015-01-30 21:37:56 +0000

    First Phase Done.

---
+++ BelArt/BelArt/Controllers/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  BelArt
+//
+//  Created by Mehdi Sqalli on 30/01/15.
+//  Copyright (c) 2015 Mehdi Sqalli. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+
+    }
+    
+}
